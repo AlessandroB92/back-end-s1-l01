83c3b667106766d75c40f4c2fc20cc3a93dc9f9c
Alessandro Basile <alessandrobasile1992@gmail.com>
2024-01-29 15:29:14 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace back_end_s1_l01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
